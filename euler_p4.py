1d36831c6862176d01f3f396f37813df26291712
Kaushik Lakshminarasimhan <jklakshm@bcm.edu>
2017-07-20 20:40:08 +0530

    Update euler_p4.py

--- euler_p4.py
+++ euler_p4.py
@@ -5,15 +5,15 @@ product of two 3-digit numbers."""
 
 # function to check if a number is factorizable as product of two three digit numbers
 def factorize3digit(num):
-    for i in range(99, 999):
+    for i in range(999, 99, -1):
         if num%i == 0 and len(str(num/i)) == 3: return True
         else: continue
     return False
 
 Nmin = 100
 Nmax = 999
-prodmax = 999*999
-prodmin = 100*100
+prodmin = Nmin**2
+prodmax = Nmax**2
 for num in range(prodmax, prodmin, -1): #IDEA: start from max and break out after success
     if str(num) == str(num)[::-1]: #IDEA: if palindrome, check if factors are three-digit numbers
         if factorize3digit(num):
